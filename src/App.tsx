aa51ca77cc41a416155901086ce7cf6803d58a7c
slobbie <haeru9410@gmail.com>
2022-07-06 03:14:23 +0900

    :tada: Start first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Mainlayout from './layout/Mainlayout';
+import Contact from './page/Contact';
+import Home from './page/Home';
+import Service from './page/Service';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<Mainlayout />}>
+          <Route index element={<Home />} />
+          <Route path='service' element={<Service />} />
+          <Route path='contact' element={<Contact />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/layout/Mainlayout.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+import Menu from '../components/Menu';
+import Header from './Header';
+
+const Mainlayout = () => {
+  return (
+    <Container>
+      <Header />
+      <MainContainer>
+        <Main>
+          <Content>
+            <OverLay>
+              <Outlet />
+            </OverLay>
+          </Content>
+        </Main>
+      </MainContainer>
+      <Menu />
+    </Container>
+  );
+};
+
+export default Mainlayout;
+
+const Container = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  background-color: #161616;
+  overflow-x: hidden;
+  transform-style: preserve-3d;
+`;
+
+const MainContainer = styled.div``;
+
+const Main = styled.div``;
+
+const Content = styled.div``;
+
+const OverLay = styled.div``;
